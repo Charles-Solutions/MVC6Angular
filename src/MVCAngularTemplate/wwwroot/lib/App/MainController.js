6f4353f6c6430f9e2fce3cbdf574bb46591bd264
Fily <fily.junior@gmail.com>
2016-02-07 19:25:54 -0500

    Create Project

---
+++ src/MVCAngularTemplate/wwwroot/lib/App/MainController.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+ServerApp.controller('MainController',['$scope','$http',function($scope, $http) {
+    
+    $scope.Version = '';
+
+    $scope.RefreshVersion = function () {
+
+       // $scope.NotifBusy = $http.get("api/DashboardApi/GetVersion");
+        //$scope.NotifBusy.success(function (result) {
+            $scope.Version = '1.0';
+        //});
+    };
+
+}])
---
+++ src/MVCAngularTemplate/wwwroot/lib/App/app.js
@@ -0,0 +1,35 @@
+﻿(function () {
+   
+    var ServerApp = angular.module('ServerApp', [
+        // Angular modules 
+        'ngRoute'
+
+        // Custom modules 
+
+        // 3rd Party Modules
+        
+    ]);
+
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/angular', {
+                templateUrl: '/Views/Home/AngularTest.html',
+                controller: 'MainController'
+            })
+         .otherwise(
+                {
+                    redirectTo: '/'
+                });
+
+
+        // $locationProvider.html5Mode(true);
+    }
+
+    ServerApp.config(config);
+
+
+
+
+
+
+})();
